fa12d2e0341e45ad80d811451961d30a444d20ec
James Gillmore <james@faceyspacey.com>
2015-11-21 14:56:54 -0800

    method-level reactivity complete!

--- packages/react-meteor-data/meteor-data-mixin.jsx
+++ packages/react-meteor-data/meteor-data-mixin.jsx
@@ -1,42 +1,40 @@
-ReactMeteorData = {
+TrackerReact = {
   componentWillMount() {
     this.bindReactiveMethods();
-    this._computations = [];
+    this._computations = {};
   },
   componentWillUnmount() {
-    this._computations.forEach(c => c.stop());
+		_.each(this._computations, c => c.stop());
     delete this._computations;
-  }
+  },
   _isFunction(functionToCheck) {
     let getType = {};
     return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-  }
+  },
   bindReactiveMethods() {
-    let reg = /props|state|conext|getDOMNode|refs/;
+    let reg = /props|state|conext|getDOMNode|refs|bindReactiveMethods|bindMethod|renderTasks/;
     
-    _.each(this, function(prop) {
+    _.each(this, function(func, prop) {
       if(this.hasOwnProperty(prop) && prop.charAt(0) !== '_' && this._isFunction(this[prop]) && !reg.test(prop)) {
         this.bindMethod(prop);
       }
     }, this);
-  }
+  },
   bindMethod(prop) {
-    let oldFunction = this[prop],
-      bindingComplete = false;
+    let oldFunction = this[prop];
           
     this[prop] = function() {
-      let computation, args = arguments;
+			if(this._computations[prop]) this._computations[prop].stop();
+      let args = arguments, response;
       
-      if(bindingComplete === false) {
-        computation = Tracker.autorun(function() {
-          oldFunction.apply(this, args);
-          this.forceUpdate();
-        }.bind(this));
-        
-        this._computations.push(computation);
-      }
-      
-      return this[prop](...args);
+			this._computations[prop] = Tracker.nonreactive(() => {
+				return Tracker.autorun(function(c) {
+	        if(c.firstRun) response = oldFunction.apply(this, args);
+	        else this.forceUpdate();
+	      }.bind(this));
+			});
+			
+      return response;
     }.bind(this);
   }
 };
